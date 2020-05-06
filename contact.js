f241b068e18cc66ab7059ff587806686646055e2
nainika-hub <55920374+nainika-hub@users.noreply.github.com>
2020-05-07 02:50:42 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function otherFunction(){
+	if (yesother.checked){
+		other.style.display="inline";
+		Other.setAttribute('required',true);
+	}
+	else{
+		Other.removeAttribute('required');
+		other.style.display="none";
+	}
+}
