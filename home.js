a07f2ded0fe307bb12416f42d6403e15a7631f5f
nainika-hub <55920374+nainika-hub@users.noreply.github.com>
2020-05-27 00:50:42 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,25 @@
+
+function myFunction(){
+	
+var x=document.getElementById("btn");
+var y =document.getElementById("btn1");
+alert(x+y);
+ if (x.style.display == "none") {
+   x.style.display = "block";
+   y.style.display = "none";
+ } else {
+   x.style.display = "none";
+   y.style.display = "block";
+ }
+}
+function myFunc() {
+var x = document.getElementById("btn");
+ var y = document.getElementById("btn1");
+ if (y.style.display == "none") {
+   y.style.display = "block";
+   x.style.display = "none";
+ } else {
+   y.style.display = "none";
+   x.style.display = "block";
+ }
+}
